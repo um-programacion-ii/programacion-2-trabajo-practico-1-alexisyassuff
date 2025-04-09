7eb01c1a39d73fa3928544d4ab272acb04e5ca64
alexisyassuff <a.yassuff@alumno.um.edu.ar>
2025-04-09 11:02:31 -0300

    Crear clase Vehiculo y clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 import java.net.SocketTimeoutException;
-import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-    public static void tester(VehiculoPrinter t) {
-        t.mostrarInformacion();
-    }
     public static void main(String[] args) {
         Vehiculo Chevrolet = new Vehiculo();
         Chevrolet.setPatente("AF2320");
@@ -27,37 +22,11 @@ public class Main {
         Fiat.setAño(2015);
         Fiat.setCapacidadCargaKg(246);
 
-        Auto MiAuto = new Auto();
-        MiAuto.setPatente("LA2129");
-        MiAuto.setMarca("Fiat");
-        MiAuto.setAño(2015);
-        MiAuto.setCapacidadCargaKg(246);
-
-        VehiculoPrinter Impresora = MiAuto;
-        Impresora.mostrarInformacion();
-
-
-        //Ford.mostrarInformacion();
-        //Fiat.mostrarInformacion();
-        //Chevrolet.mostrarInformacion();
-
-
-        Camion Scania = new Camion();
-        Scania.setAño(2020);
-        Scania.setMarca("Scania");
-        Scania.setPatente("KD2342");
-        Scania.setCapacidadCargaKg(100000);
-        Scania.setTieneAcoplado(true);
-
-        Impresora = Scania;
-        Impresora.mostrarInformacion();
-        System.out.println("testeando LISKOV");
-        tester(Scania);
-        tester(MiAuto);
+        Ford.mostrarInformacion();
+        Fiat.mostrarInformacion();
+        Chevrolet.mostrarInformacion();
 
     }
 
 
-
 }
-
--- src/VehiculoPrinter.java
+++
@@ -1,3 +0,0 @@
-interface VehiculoPrinter {
-    void mostrarInformacion();
-}
